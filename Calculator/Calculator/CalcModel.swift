d097f35f758ae67dbd44c7d105e6c0d728389d15
AnuarBei <72887906+AnuarBei@users.noreply.github.com>
2021-02-11 14:04:08 +0600

    Add files via upload

---
+++ Calculator/Calculator/CalcModel.swift
@@ -0,0 +1,67 @@
+//
+//  CalcModel.swift
+//  Calculator
+//
+//  Created by Ануар Беисов on 11.02.2021.
+//
+
+import Foundation
+
+enum Operation{
+    case constant(Double)
+    case unaryOperation((Double)->Double)
+    case binaryOperation((Double,Double)->Double)
+    case equals
+}
+
+
+struct CalculatorModel{
+    var my_operation: Dictionary<String,Operation> =
+    [
+        "∏": Operation.constant(Double.pi),
+        "℮": Operation.constant(M_E),
+        "√": Operation.unaryOperation(sqrt),
+        "+": Operation.binaryOperation({$0+$1}),
+        "-": Operation.binaryOperation({$0-$1}),
+        "*": Operation.binaryOperation({$0*$1}),
+        "/": Operation.binaryOperation({$0/$1}),
+        "=": Operation.equals
+    ]
+    private var global_value: Double?
+    
+    mutating func setOperand(_ operand: Double){
+        global_value = operand
+    }
+    mutating func performOperation(_ operation: String){
+        let symbol = my_operation[operation]!
+        switch symbol {
+        case .constant(let value):
+            global_value = value
+            
+        case .unaryOperation(let function):
+            global_value = function(global_value!)
+            
+        case .binaryOperation(let function):
+            saving = SaveFirstOperandAndOperation(firstOperand: global_value!, operation: function)
+        case.equals:
+            if global_value != nil{
+            
+                global_value = saving?.performOperationWith(secondOperand: global_value!)
+            }
+        }
+    }
+    var result: Double?{
+        get{
+            return global_value
+        }
+    }
+    private var saving: SaveFirstOperandAndOperation?
+    struct SaveFirstOperandAndOperation {
+        var firstOperand: Double
+        var operation: (Double,Double)-> Double
+        
+        func performOperationWith(secondOperand op2: Double) -> Double{
+            return operation(firstOperand,op2)
+        }
+    }
+}
